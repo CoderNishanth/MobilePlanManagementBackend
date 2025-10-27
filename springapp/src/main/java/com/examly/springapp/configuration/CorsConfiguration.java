3b8a92caf491b0a0c0e722c874e1f755b629b830
nishanthcoder21@gmail.com <727723euai079@skcet.ac.in>
2025-10-27 12:38:29 +0530

    Frontend url added

--- springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
+++ springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("https://8081-fbeecedcbabbdbbdfafcfffbdacddbfcdceaaffcc.premiumproject.examly.io", "http://localhost:3000", "http://localhost:5173") 
+        .allowedOrigins("https://mobileplanmanagement.web.app", "http://localhost:5173") 
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         .allowedHeaders("*")
         .allowCredentials(true)
